138c4883bc07e470a580c8c58e11d75e47f5e5ef
your-user-name <your@email.com>
2025-04-13 18:51:08 -0600

    trabajo practico 4 v3

--- retraining.py
+++ retraining.py
@@ -11,7 +11,7 @@ import joblib
 # --------------------------------------------
 # READ THE DATASET
 # --------------------------------------------
-df = pd.read_csv("Data/data.csv")
+df = pd.read_csv("data/data.csv")
 
 # --------------------------------------------
 # PREPROCESSING

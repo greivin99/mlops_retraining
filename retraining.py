1f6ea7721f90c00c184e96f594aa3615825660fb
your-user-name <your@email.com>
2025-04-13 18:28:23 -0600

    fixed

--- retraining.py
+++ retraining.py
@@ -11,7 +11,7 @@ import joblib
 # --------------------------------------------
 # READ THE DATASET
 # --------------------------------------------
-df = pd.read_csv("data/data.csv")
+df = pd.read_csv("../Trabajo practico 4/Data/data.csv")
 
 # --------------------------------------------
 # PREPROCESSING
